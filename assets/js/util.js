b3f4431a3c6e4bff0a778b4f03b1667966c6264b
Tearnote <marach5@gmail.com>
2023-01-13 15:24:27 +0000

    Document util.js

--- assets/js/util.js
+++ assets/js/util.js
@@ -1,4 +1,9 @@
+// util.js
+// Random useful utility functions
+
 class Util {
+	// Convert strings with uppercase letters to kebab case
+	// eg. FooBar => -foo-bar
 	// https://stackoverflow.com/a/47836484
 	static kebabCase(str) {
 		return str.replace(/[A-Z]/g, (m) => "-" + m.toLowerCase());
